3924880ce3ad74e5dacedb8bac5df09c786e1367
JacobMace <xds513@york.ac.uk>
2025-10-21 13:04:58 +0100

    Add files via upload
    Updated player class with speedUp method.

--- core/src/main/java/com/escapefromuni/main/Player.java
+++ core/src/main/java/com/escapefromuni/main/Player.java
@@ -12,6 +12,7 @@ public class Player {
     Sprite playerSprite;
     // Player speed attribute makes it possible to alter speed during the game.
     float playerSpeed = 200f;
+    float speedTimer = 0;
 
     public Player(){
         // Player texture currently hardcoded as player.png as there is only one texture
@@ -23,6 +24,15 @@ public class Player {
     public void movePlayer(){
         // Using the dt function makes the speed of the player constant on different hardware.
         float dt = Gdx.graphics.getDeltaTime();
+        // Check that the player has time left on their speed-up.
+        if(speedTimer > 0){
+            speedTimer -= dt;
+        }
+        // If player is out of time reset the speed and ensure counter is at 0.
+        else if(speedTimer <= 0){
+            speedTimer = 0;
+            playerSpeed = 200f;
+        }
         // Check which keys are being pressed on each frame and move the sprite accordingly.
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
             playerSprite.translateX(playerSpeed * dt);
@@ -37,6 +47,11 @@ public class Player {
             playerSprite.translateY(-playerSpeed*dt);
         }
 
+    }
+
+    public void speedUp(){
+        speedTimer += 30;
+        playerSpeed = 3000f;
     }
 
 }
