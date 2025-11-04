e3eb17ecee4a435c3df0580e5535060349c69044
Joseph Hinde <56842156+ShamansGit@users.noreply.github.com>
2025-11-04 15:04:42 +0000

    Camera system and Main refactoring

--- core/src/main/java/com/escapefromuni/main/Main.java
+++ core/src/main/java/com/escapefromuni/main/Main.java
@@ -3,12 +3,14 @@ package com.escapefromuni.main;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.escapefromuni.main.components.RenderableComponent;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -16,33 +18,36 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     Sprite titleImage;
     Sprite playButton;
-    Player player;
-    GameMap map;
+    //The camera which is active and rendering the scene
+    CameraController activeCamera;
     String gameState = "title";
-    OrthographicCamera GameCamera;
-    //TODO: move player into this list (and all future GameObjects)
     List<GameObject> gameObjects;
-    //TODO: add separate list of GameObjects with renderable component etc.
+    List<RenderableComponent> renderableComponents;
     @Override
     public void create() {
         batch = new SpriteBatch();
-        titleImage = new Sprite(new Texture(Gdx.files.internal("titleGraphic.png")));
+        gameObjects = new ArrayList<GameObject>();
+        renderableComponents = new ArrayList<RenderableComponent>();
+        //Create the Main Menu UI and position it
+        titleImage = new Sprite(new Texture(Gdx.files.internal("player.png")));
         titleImage.setPosition((Gdx.graphics.getWidth() - titleImage.getTexture().getWidth()) / 2f, (Gdx.graphics.getHeight() * 1.5f - titleImage.getTexture().getHeight()) / 2f);
-        playButton = new Sprite(new Texture(Gdx.files.internal("playButton.png")));
+        playButton = new Sprite(new Texture(Gdx.files.internal("player.png")));
         playButton.setPosition((Gdx.graphics.getWidth() - playButton.getTexture().getWidth()) / 2f, (Gdx.graphics.getHeight() / 2f - playButton.getTexture().getHeight()) / 2f);
-        // Define a camera which the game will be seen through. This is currently centered around (0,0)
-        GameCamera = new OrthographicCamera();
-        GameCamera.setToOrtho(false,540,360);
-        // The TileMap provided requires a camera to function.
-        map = new GameMap(GameCamera);
-        //TODO: Iterate through every GameObject and run the start() function on them
-        player = new Player(new Vector2(0,0));
-        player.start();
+        //Create the Game World:
+        //Define a camera and add it to GameObjects
+        activeCamera = new CameraController(null);
+        addGameObject(activeCamera);
+
+        //Add the player
+        var player = addGameObject(new Player(new Vector2(20,20)));
+        activeCamera.SetTarget(player);
+        //Add the Map
+        addGameObject(new GameMap(activeCamera));
     }
 
     @Override
     public void render() {
-        if (gameState == "title") {
+        if (gameState.equals("title")) {
             //TODO: hardcoded to default screen size, fix later
             if (Gdx.input.getX() >= 340 && Gdx.input.getX() <= 740 && 
                 Gdx.input.getY() >= 440 && Gdx.input.getY() <= 640 && 
@@ -55,29 +60,48 @@ public class Main extends ApplicationAdapter {
             playButton.draw(batch);
             batch.end();
         }
-        //TODO: Iterate through every GameObject with the RenderableComponent and run render()
-        //TODO: Camera system / Camera position
-        else if (gameState == "main") {
+        else if (gameState.equals("main")) {
             ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-            batch.begin();
-            map.RenderMap();
-            player.update(Gdx.graphics.getDeltaTime());
-            if(Gdx.input.isKeyPressed(Input.Keys.F)){
-                player.speedUp();
+            //cache the delta time
+            float deltaTime = Gdx.graphics.getDeltaTime();
+            //Update all gameObjects
+            for (var gameObject : gameObjects) {
+                gameObject.update(deltaTime);
             }
-            //TODO: Change cameraPosition to a GameObject with a CameraComponent to control easier
-            Vector2 cameraPosition = new Vector2(-Gdx.graphics.getWidth() / 2f,-Gdx.graphics.getHeight() / 2f);
-            player.render(batch,cameraPosition);
+            //set view to active camera
+            batch.setProjectionMatrix(activeCamera.GetCamera().combined);
+            //For all gameObjects that render, render them
+            batch.begin();
+            for (var renderable : renderableComponents) {
+                //todo cameraPosition is obsolete, which is why its (0,0)
+                renderable.render(batch,new Vector2(0,0));
+            }
             batch.end();
         }
-        
     }
 
     @Override
     public void dispose() {
-        //TODO: run dispose() on every GameObject in gameObjects
-        player.dispose();
         batch.dispose();
-        map.dispose();
+        //dispose every gameObject
+        for (var gameObject : gameObjects) {
+            gameObject.dispose();
+        }
+    }
+
+    /**
+     * Adds a gameObject to the game world and runs the start() procedure.
+     * @param gameObject The gameobject to add.
+     * @return the gameObject added if successful, null if not.
+     */
+    //todo: return null if fail
+    public GameObject addGameObject(GameObject gameObject){
+        gameObjects.add(gameObject);
+        //If the object has a renderableComponent, then add it to the list of renderableComponents
+        if (gameObject instanceof RenderableComponent) {
+            renderableComponents.add((RenderableComponent) gameObject);
+        }
+        gameObject.start();
+        return gameObject;
     }
 }
