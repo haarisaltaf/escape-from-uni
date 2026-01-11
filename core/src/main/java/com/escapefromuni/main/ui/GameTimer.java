747e668f78e5ca952376fd8c6bcd130225fb4827
sati5M <diyarsavda@gmail.com>
2026-01-11 03:00:17 +0000

    Add pause timer method (Needs to be added to floor/however we plan to activate it)

--- core/src/main/java/com/escapefromuni/main/ui/GameTimer.java
+++ core/src/main/java/com/escapefromuni/main/ui/GameTimer.java
@@ -3,6 +3,7 @@ package com.escapefromuni.main.ui;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Timer;
 import com.escapefromuni.main.Game;
 
 public class GameTimer extends UIElement {
@@ -11,8 +12,9 @@ public class GameTimer extends UIElement {
     float time;
     BitmapFont font;
     int eventCounter = 0;
+    boolean timerPaused = false;
 
-    public GameTimer(Vector2 relativeScreenPosition){
+    public GameTimer(Vector2 relativeScreenPosition) {
         super(relativeScreenPosition);
         time = 300;
         font = new BitmapFont();
@@ -20,13 +22,14 @@ public class GameTimer extends UIElement {
 
     /**
      * If the game time drops below zero the GameState is set to lose.
+     * 
      * @param deltaTime : The time in ms since the last update
      */
     @Override
-    public void update(float deltaTime){
-        if(Game.gameState == Game.GameState.PLAYING) {
+    public void update(float deltaTime) {
+        if (Game.gameState == Game.GameState.PLAYING && timerPaused == false) {
             time -= deltaTime;
-            if (time < 0){
+            if (time < 0) {
                 Game.gameState = Game.GameState.LOSE;
             }
         }
@@ -34,36 +37,49 @@ public class GameTimer extends UIElement {
 
     /**
      * Converts a time given in seconds to minutes and seconds instead.
+     * 
      * @param timeSeconds The input time in seconds.
      * @return A formatted string in the form MM:SS
      */
-    public static String GetTimeString(float timeSeconds){
+    public static String GetTimeString(float timeSeconds) {
         float minutes = (timeSeconds % 3600) / 60;
         float seconds = timeSeconds % 60;
 
-        return String.format("%02d:%02d", (int)Math.floor(minutes), (int)Math.floor(seconds));
+        return String.format("%02d:%02d", (int) Math.floor(minutes), (int) Math.floor(seconds));
     }
 
     // Returns the time left on the timer
-    public float GetTime(){
+    public float GetTime() {
         return time;
     }
 
     // Adds 5 seconds to timer
-    public void addTime(float seconds){
-	time += seconds;
+    public void addTime(float seconds) {
+        time += seconds;
     }
 
-    //Render the timer
+    // Pause timer
+    public void pauseTimer(float seconds) {
+        System.out.println("We here");
+        timerPaused = true;
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                timerPaused = false;
+            }
+        }, seconds);
+    }
+
+    // Render the timer
     @Override
     public void render(SpriteBatch batch) {
-        font.draw(batch,"Time Left : " + GetTimeString(time) + "\n Events : " + eventCounter, position.x,position.y);
+        font.draw(batch, "Time Left : " + GetTimeString(time) + "\n Events : " + eventCounter, position.x, position.y);
     }
 
     // Set position on screen
     @Override
-    public void positionOnScreen(Vector2 cameraPosition, Vector2 screenSize,float zoom) {
-        super.positionOnScreen(cameraPosition,screenSize,zoom);
+    public void positionOnScreen(Vector2 cameraPosition, Vector2 screenSize, float zoom) {
+        super.positionOnScreen(cameraPosition, screenSize, zoom);
         font.getData().setScale(3 * zoom);
     }
 
@@ -71,4 +87,5 @@ public class GameTimer extends UIElement {
     public void setEventCounter(int eventCounter) {
         this.eventCounter = eventCounter;
     }
+
 }
