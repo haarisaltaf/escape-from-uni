05e557d9e048d393aa75043587917ab447170637
haarisaltaf <altafhaaris@gmail.com>
2026-01-11 21:57:26 +0000

    template for testing

---
+++ core/src/test/java/com/escape-from-uni/ScoreTest.java
@@ -0,0 +1,16 @@
+package com.escape-from-uni;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreTest {
+
+    @Test
+    void testScoreCalculation() {
+    }
+
+    @Test
+    void testScoreNotNegative() {
+    }
+}
